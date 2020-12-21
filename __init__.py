b3f04dd7e583cf9d84d8967f1f9e45410b7a3d32
kaixindelele <1985790413@qq.com>
2020-12-21 21:54:06 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 try:
     from ddpg_sp.DDPG_per_class import DDPG as DDPG
-    from sac_auto.sac_auto_per_class import SAC as SAC
-    from td3_sp.TD3_per_class import TD3 as TD3    
+    from sac_auto.sac_auto_per_class import SAC as SAC_AUTO
+    from td3_sp.TD3_per_class import TD3 as TD3
+    from sac_sp.sac_class import SAC as SAC
 except:
     from rl_algorithms.ddpg_sp.DDPG_per_class import DDPG as DDPG
-    from rl_algorithms.sac_auto.sac_auto_per_class import SAC as SAC
+    from rl_algorithms.sac_auto.sac_auto_per_class import SAC as SAC_AUTO
     from rl_algorithms.td3_sp.TD3_per_class import TD3 as TD3
+    from rl_algorithms.sac_sp.sac_class import SAC as SAC
     
